6def6df98056f30203f44686fffc84c69c150c60
Subhashree <subhashreerajait@gmail.com>
2026-04-01 06:24:06 +0530

    First commit in Selenium basics

---
+++ Selenium_basics/src/test/java/demo_test.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.*;
+
+
+
+public class demo_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in");
+		WebElement search=driver.findElement(By.name("q"));
+		search.sendKeys("Selenium",Keys.ENTER);
+		System.out.println("Page title is:"+driver.getTitle());
+		driver.close();
+		
+
+	}
+
+}
